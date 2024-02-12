09f14aae734f55cb801654170db6e0d8ef1dbec6
Rohit Kumar Ranjan <119317461+Rohitkrranjan@users.noreply.github.com>
2024-02-12 20:29:44 +0530

    Add files via upload

---
+++ Reverse_String_Recursion.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+void reverse(string &str , int i,int j)
+{
+	// base case 
+	 if(i>j)
+	  return ;
+	swap(str[i], str[j]);
+	i++;
+	j--;
+	
+	// recursive call
+	 reverse(str,i,j);
+}
+
+int main(){
+	string name = "Ranjan";
+	reverse(name,0,name.length()-1);
+	
+	cout<<endl;
+	cout<<name;
+}
